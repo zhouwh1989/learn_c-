7d8517d26cab3bd55592bf21b386d4aeedce68fa
zhouwh <vanora.zhou@gmail.com>
2018-01-15 21:42:14 +0800

    文件读写 streamreader/streamwriter/binaryreader/binarywriter savefiledialog convert/bitconvert

--- 基本语法/Form1.Designer.cs
+++ 基本语法/Form1.Designer.cs
@@ -32,6 +32,13 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -67,11 +74,73 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(99, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Write";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 227);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "测试文件读写";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 290);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "SaveFile";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "saveFileDialog";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 388);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -89,6 +158,13 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
 
 
     }
--- 基本语法/Form1.cs
+++ 基本语法/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using MyNameSpace;
+// 文件操作
+using System.IO;
+
 
 namespace winFormProj
 {
@@ -29,12 +32,38 @@ namespace winFormProj
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //convert Bitconvert 基础类型转换，区别在于BitConvert是将基础类型转为byte数组
+
+            // is as casting
+            // is 关键字用于判断是否属于某种类型
+            // as/casting 做类型转换
+            // example
+            //object myObject = "stringTest";
+            //if (myObject is string) MessageBox.Show(myObject as string);
+
             // openfileDialogs
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "文本|*.txt|图片|*.jpg";
             ofd.Title = "OpenFileDialogTest";
             if (ofd.ShowDialog() == DialogResult.OK)
             {
+                
+                StreamReader sr = new StreamReader(File.OpenRead(ofd.FileName));
+                char c = (char)sr.Peek();   //获取一个字符，但是不改变文件偏移
+                char c1 = (char)sr.Read();  // 读取一个字符，文件偏移+1
+                char c2 = (char)sr.Read();
+
+                //// streamreader
+                //textBox1.Text = sr.ReadToEnd(); // 二进制方式读取整个文件
+                textBox1.Text = sr.BaseStream.ReadByte().ToString();    // 读取十六进制字节，转成ASCII码
+                byte [] buffer = new byte[5];
+                sr.BaseStream.Position = 2; // 指定offset
+                sr.BaseStream.Read(buffer, 0, 5);   // 读取指定长度的流
+                foreach (byte b in buffer)
+                {
+                    textBox1.Text += b.ToString("X") + " ";
+                }
+                sr.Dispose();
                 MessageBox.Show(ofd.FileName);
             }
 
@@ -106,5 +135,82 @@ namespace winFormProj
         {
 
         }
+
+        ///////////// streamWriter
+        string path;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // openfileDialog
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "文本|*.txt|图片|*.jpg";
+            ofd.Title = "OpenFileDialogTest";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;
+                button3.Enabled = true;
+            }
+            ofd.Dispose();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /// streamwriter
+            //StreamWriter sw =Q new StreamWriter(File.Create(path));
+            //sw.WriteLine("I am a new line");
+            //sw.Flush();
+            ////sw.Write(textBox2.Text);
+            //sw.BaseStream.Position = 0x2B;
+            //sw.BaseStream.WriteByte(0xff);
+            //sw.Dispose();
+
+
+            ////BinaryReader
+            //BinaryReader br = new BinaryReader(File.OpenRead(path));
+            ////textBox2.Text = br.ReadChar().ToString();
+            
+            //// binaryReader的ReadInt16优先读取文件偏移高的字节,可以认为是小端模式
+            //textBox2.Text = "";
+
+            ////textBox2.Text += br.ReadInt16().ToString("x");
+            ////textBox2.Text += "|";
+
+            //byte [] buffer = br.ReadBytes(2);
+            //foreach (byte b in buffer)
+            //{
+            //    textBox2.Text += b.ToString("x");
+            //    textBox2.Text += " ";
+            //}
+            //textBox2.Text += BitConverter.ToInt16(buffer, 0).ToString("x"); // BitConverter.toInt16也是按小端读取
+            //br.Dispose();
+
+            BinaryWriter bw = new BinaryWriter(File.Create(path));
+            short myShort = 1;
+            byte[] buffer = BitConverter.GetBytes(myShort);
+            bw.Write(buffer);
+            //bw.BaseStream.WriteByte(0x12);
+            bw.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text File|*.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                
+                string path = sfd.FileName;
+                BinaryWriter bw = new BinaryWriter(File.Create(path));
+                byte [] buffer= new byte[5];
+                for(int i=0; i<5; i++)
+                {
+                    textBox2.Text += ((byte)i).ToString("x");
+                    textBox2.Text += " ";
+                    buffer[i] = (byte)i;
+                }
+                bw.BaseStream.Write(buffer, 0, 5);
+                //bw.Write("SaveFileDialog Test.");
+                bw.Dispose();
+            }
+        }
+        //////////////////
     }
 }
